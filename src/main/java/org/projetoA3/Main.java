b5f3c003c9c41b329fd2395e0daf39b32ad19b1c
juniorbouani <juniorbouani@gmail.com>
2026-05-10 16:26:09 -0300

    Semantic increase and main att

--- src/main/java/org/projetoA3/Main.java
+++ src/main/java/org/projetoA3/Main.java
@@ -3,10 +3,12 @@ package org.projetoA3;
 import org.projetoA3.ast.ProjetoNode;
 import org.projetoA3.excessoes.FileException;
 import org.projetoA3.excessoes.LexicalException;
+import org.projetoA3.excessoes.SemanticException;
 import org.projetoA3.excessoes.SyntaxException;
 import org.projetoA3.lexer.Scan;
 import org.projetoA3.lexer.Token;
 import org.projetoA3.parser.Parser;
+import org.projetoA3.semantic.SemanticAnalyzer;
 
 
 import java.io.BufferedReader;
@@ -20,47 +22,68 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Digite o caminho do arquivo: ");
-        String caminho = scan.nextLine();
+        System.out.println("Deseja iniciar? (s/n): ");
+        char escolha = scan.nextLine().trim().charAt(0);
 
-        try (BufferedReader br = new BufferedReader(new FileReader(caminho))) {
+        while (escolha == 's' || escolha == 'S') {
 
-            StringBuilder sb = new StringBuilder();
+            System.out.println("Quantos arquivos deseja ler: ");
+            int n = scan.nextInt();
+            scan.nextLine();
 
-            String linha;
-            while ((linha = br.readLine()) != null) {
-                sb.append(linha).append("\n");
+            for (int i = 1; i <= n; i++) {
+
+
+                System.out.println("Digite o caminho do arquivo: ");
+                String caminho = scan.nextLine();
+
+                try (BufferedReader br = new BufferedReader(new FileReader(caminho))) {
+
+                    StringBuilder sb = new StringBuilder();
+
+                    String linha;
+                    while ((linha = br.readLine()) != null) {
+                        sb.append(linha).append("\n");
+                    }
+                    String code = sb.toString();
+
+                    Scan scanner = new Scan(code);
+                    List<Token> tokens = scanner.tokenize();
+                    tokens.forEach(System.out::println);
+
+                    Parser parser = new Parser(tokens);
+                    ProjetoNode projeto = parser.parse();
+
+                    SemanticAnalyzer semantic = new SemanticAnalyzer(projeto);
+                    semantic.analisar();
+
+                    System.out.println(projeto);
+
+
+                } catch (LexicalException e) {
+                    System.err.println(e.getMessage());
+                } catch (SyntaxException e) {
+                    System.err.println(e.getMessage());
+                } catch (FileNotFoundException e) {
+                    throw new FileException("Arquivo não encotrado: " + caminho);
+                } catch (IOException e) {
+                    throw new FileException("Falha ao ler o arquivo: " + e.getMessage());
+                } catch (SecurityException e) {
+                    throw new FileException("Sem permissão para ler: " + caminho);
+                } catch (FileException e) {
+                    System.err.println(e.getMessage());
+                } catch (SemanticException e) {
+                    System.err.println(e.getMessage());
+                }
+                scan.nextLine();
             }
-            String code = sb.toString();
-
-            Scan scanner = new Scan(code);
-            List<Token> tokens = scanner.tokenize();
-            tokens.forEach(System.out::println);
-
-            Parser parser = new Parser(tokens);
-            ProjetoNode projeto = parser.parse();
-            System.out.println(projeto);
+            System.out.println("Deseja analisar mais arquivos? (s/n):");
+            escolha = scan.nextLine().trim().charAt(0);
+            scan.nextLine();
         }
-        catch (LexicalException e ) {
-            System.err.println(e.getMessage());
-        }
-        catch (SyntaxException e) {
-            System.err.println(e.getMessage());
-        }
-        catch (FileNotFoundException e) {
-            throw new FileException("Arquivo não encotrado: " + caminho);
-        }
-        catch (IOException e) {
-            throw new FileException("Falha ao ler o arquivo: " + e.getMessage());
-        }
-        catch (SecurityException e ) {
-            throw new FileException("Sem permissão para ler: " + caminho);
-        }
-        catch (FileException e) {
-            System.err.println(e.getMessage());
-        }
+        System.out.println("Encerrando o SecureCode Audit. Até logo!");
     }
 }
---
+++ src/main/java/org/projetoA3/semantic/SemanticAnalyzer.java
@@ -0,0 +1,76 @@
+package org.projetoA3.semantic;
+
+import org.projetoA3.ast.ProjetoNode;
+import org.projetoA3.ast.RegraNode;
+import org.projetoA3.excessoes.SemanticException;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SemanticAnalyzer {
+
+    private final ProjetoNode projeto;
+    private final Set<String> nomesUsados = new HashSet<>();
+
+
+    public SemanticAnalyzer(ProjetoNode projeto) {
+        this.projeto = projeto;
+    }
+
+    public void analisar() {
+        for (RegraNode regra : projeto.getRegras()) {
+            validarNomeDuplicado(regra);
+            validarCondicoes(regra);
+            validarNivelAcao(regra);
+
+        }
+        System.out.println("SEMANTICO: Analise concluida sem erros!");
+    }
+
+    private void validarNomeDuplicado(RegraNode regra) {
+        if (nomesUsados.contains(regra.getNome())) {
+            throw new SemanticException(
+                    "Regra duplicada: '" + regra.getNome() + "' já foi denifinida anteriormente."
+            );
+        }
+        nomesUsados.add(regra.getNome());
+
+    }
+
+    private void validarCondicoes(RegraNode regra) {
+        if ( regra.getCondicoes().isEmpty()) {
+            throw new SemanticException(
+                    "Regra '" + regra.getNome() + "' não possui nenhuma condição."
+            );
+        }
+
+    }
+
+    private void validarNivelAcao(RegraNode regra) {
+        String nivel = regra.getNivel();
+        String acao = regra.getAcao();
+
+        switch (nivel) {
+            case "BAIXA" -> {
+                if (acao.equals("REJEITAR") || acao.equals("SOLICITAR_MFA")) {
+                    throw new SemanticException("Regra '" + regra.getNome() + "': nível BAIXA não pode ter ação " + acao +
+                            ". " + "Use NOTIFICAR ou LOG_ERROR."
+                    );
+                }
+
+            }
+            case "MEDIA" -> {
+                if (acao.equals("REJEITAR")) {
+                    throw new SemanticException("Regra '" + regra.getNome() + "': nível MEDIA não pode ter ação REJEITAR" +
+                            "Use NOTIFICAR, LOG_ERROR ou SOLICITAR_MFA");
+
+                }
+            }
+
+        }
+
+
+    }
+
+
+}
